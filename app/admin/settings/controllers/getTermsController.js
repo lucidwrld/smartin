18d69e08fe5415018e49bb069f117e55b5918b62
Ifeanyi Ejindu <ifeanyiejindu@gmail.com>
2025-01-08 09:42:08 +0100

    update

--- app/admin/settings/controllers/getTermsController.js
+++ app/admin/settings/controllers/getTermsController.js
@@ -1,13 +1,12 @@
 import { useQuery } from "react-query";
-import AxiosWithToken from "@/constants/api_management/MyHttpHelperWithToken";
-import { toast } from "react-toastify";
+import Axios from "@/constants/api_management/MyHttpHelper";
 
 const useGetTermsManager = ({ type }) => {
   return useQuery({
     queryKey: ["terms", type],
     queryFn: async () => {
       try {
-        const response = await AxiosWithToken.get(`/terms/${type}`);
+        const response = await Axios.get(`/terms/${type}`);
         return response.data;
       } catch (error) {
         throw new Error("Sorry: " + error.response?.data?.message);
