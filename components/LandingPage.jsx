7b7fb3c575f5540e4e6fc42b14723709c9b0ec3f
Ifeanyi Ejindu <ifeanyiejindu@gmail.com>
2025-07-18 08:10:16 +0100

    hide pricing

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -528,7 +528,7 @@ const LandingPage = () => {
           </div>
         </section>
 
-        <section id="pricing" className="py-16">
+        {/* <section id="pricing" className="py-16">
           <div className="max-w-6xl mx-auto px-4">
             <div className="text-center mb-16">
               <h2 className="text-3xl md:text-4xl font-bold mb-4">
@@ -601,7 +601,7 @@ const LandingPage = () => {
               ))}
             </div>
           </div>
-        </section>
+        </section> */}
 
         <section
           id="demo"
